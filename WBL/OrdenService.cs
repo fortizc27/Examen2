535d0df73d744570e75860febec2d831dae31def
fortiz <fortiz@gbm.net>
2021-11-20 21:25:55 -0600

    .

---
+++ WBL/OrdenService.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BD;
+using Entity;
+
+namespace WBL
+{
+    public interface IOrdenService
+    {
+        Task<DBEntity> Create(OrdenEntity entity);
+        Task<DBEntity> Delete(OrdenEntity entity);
+        Task<IEnumerable<OrdenEntity>> Get();
+        Task<OrdenEntity> GetById(OrdenEntity entity);
+        Task<DBEntity> Update(OrdenEntity entity);
+    }
+
+    public class OrdenService : IOrdenService
+    {
+        private readonly IDataAccess sql;
+
+        public OrdenService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+        #region MetodosCrud
+
+        //Get
+        public async Task<IEnumerable<OrdenEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<OrdenEntity>("OrdenObtener");
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //GetById
+        public async Task<OrdenEntity> GetById(OrdenEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<OrdenEntity>("OrdenObtener", new { entity.IdOrden });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Create
+        public async Task<DBEntity> Create(OrdenEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("OrdenInsertar", new
+                {
+                    entity.IdProducto,
+                    entity.CantidadProducto,
+                    entity.Estado
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Update
+        public async Task<DBEntity> Update(OrdenEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("OrdenActualizar", new
+                {
+                    entity.IdOrden,
+                    entity.IdProducto,
+                    entity.CantidadProducto,
+                    entity.Estado
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Delete
+        public async Task<DBEntity> Delete(OrdenEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("OrdenEliminar", new
+                {
+                    entity.IdOrden
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ WBL/ProductoService.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BD;
+using Entity;
+
+
+namespace WBL
+{
+    public interface IProductoService
+    {
+        Task<DBEntity> Create(ProductoEntity entity);
+        Task<DBEntity> Delete(ProductoEntity entity);
+        Task<IEnumerable<ProductoEntity>> Get();
+        Task<IEnumerable<ProductoEntity>> Lista();
+        Task<ProductoEntity> GetById(ProductoEntity entity);
+        Task<DBEntity> Update(ProductoEntity entity);
+    }
+
+    public class ProductoService : IProductoService
+    {
+        private readonly IDataAccess sql;
+
+        public ProductoService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+        #region MetodosCrud
+
+        //Get
+        public async Task<IEnumerable<ProductoEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<ProductoEntity>("ProductoObtener");
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Lista
+        public async Task<IEnumerable<ProductoEntity>> Lista()
+        {
+            try
+            {
+                var result = sql.QueryAsync<ProductoEntity>("ProductoLista");
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //GetById
+        public async Task<ProductoEntity> GetById(ProductoEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<ProductoEntity>("ProductoObtener", new { entity.IdProducto });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Create
+        public async Task<DBEntity> Create(ProductoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("ProductoInsertar", new
+                {
+                    entity.NombreProducto,
+                    entity.PrecioProducto
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Update
+        public async Task<DBEntity> Update(ProductoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("ProductoActualizar", new
+                {
+                    entity.IdProducto,
+                    entity.NombreProducto,
+                    entity.PrecioProducto
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Delete
+        public async Task<DBEntity> Delete(ProductoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("ProductoEliminar", new
+                {
+                    entity.IdProducto
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        #endregion
+    }
+}
