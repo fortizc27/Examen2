ffa3a12c9b4f55322e459b98a4d75da5e43e1c2b
fortiz <fortiz@gbm.net>
2021-11-20 20:27:34 -0600

    Add project files.

---
+++ Database/StoredProcedure/OrdenObtener.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[OrdenObtener]
+	@IdOrden int = NULL
+
+AS BEGIN
+SET NOCOUNT ON
+
+	SELECT
+		O.IdOrden,
+		O.CantidadProducto,
+		O.Estado,
+		O.IdProducto,
+		
+		P.NombreProducto
+	FROM Orden O
+	INNER JOIN Producto P
+	ON O.IdProducto = P.IdProducto
+	WHERE 
+	(@IdOrden IS NULL OR IdOrden = @IdOrden)
+
+END
+
+
+GO
---
+++ Database/StoredProcedure/ProductoObtener.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[ProductoObtener]
+	@IdProducto int = NULL
+
+AS BEGIN
+SET NOCOUNT ON
+
+	SELECT
+		IdProducto,
+		NombreProducto,
+		PrecioProducto
+	FROM Producto
+	WHERE
+	(@IdProducto IS NULL OR IdProducto=@IdProducto)
+END
+
+GO
